7e563ba22c97e06a57976bf4286ddd58eb3245f7
Vova <vovbi4@gmail.com>
2024-07-23 18:15:00 -0700

    added part 2

--- advent_of_code/Day 04/part2.py
+++ advent_of_code/Day 04/part2.py
@@ -10,24 +10,31 @@ def get_score (input_string):
     scratched_numbers = scratched_str.split(' ')
     winning_numbers = winning_str.split(' ')
 
-
     score = [i for i in scratched_numbers if i in winning_numbers]
     score = [num for num in score if num]
 
     if len(score) <= 0:
         final_score = 0
     else:   
-        final_score = pow(2, len(score) - 1)
-    print(f"Final score for {card_str}: {final_score}")
+        final_score = len(score)
     return final_score
 
-total_final_score = 0
+total_cards = 0
 
 i = 0
+number_of_cards = len(data)
+pile_of_cards = [1] * number_of_cards
 while i < len(data):
     input_string = data[i].strip()
     final_score = get_score(input_string)
-    total_final_score += final_score
-    i += 1
+    j = final_score
+    while j > 0:
+        pile_of_cards[i + j] += 1
+        j -= 1
+    print(f"Pile of cards updated\n")
+    total_cards += 1
+    pile_of_cards[i] -= 1
+    if pile_of_cards[i] == 0:
+        i += 1
     
-print(f"Total final score: {total_final_score}")
+print(f"Total cards: {total_cards}")
